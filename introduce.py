80b28a49dd0761ae108c2b423c423934d99d0ea5
Bailin <libailin1989@hotmail.com>
2016-10-30 14:48:35 +0800

    Add ask_name and say_bye by user1

--- introduce.py
+++ introduce.py
@@ -1,2 +1,10 @@
 def introduce():
     print("My name is Brian")
+
+def ask_name():
+    print("Your name?")
+    s1 = sys.stdin.readline()
+    return s1
+
+def say_bye():
+    print("See you next time!")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     print("Hello World!")
     introduce.introduce()
     print("Nice to meet you!")
+    introduce.say_bye()
 
 if __name__ == "__main__":
     main()
